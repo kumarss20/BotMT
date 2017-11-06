1c3994b8c4502dc89776158c1c54275cad099f5e
Kumar Subbiah Shunmugathai (MINDTREE LIMITED) <v-kusubb@microsoft.com>
2017-11-06 12:33:22 -0800

    changed the order of workflow

--- app.js
+++ app.js
@@ -3,22 +3,22 @@ require('dotenv-extended').load();
 
 var builder = require('botbuilder');
 var restify = require('restify');
+//const express = require('express');
 var Connection = require('tedious').Connection;
 var Request = require('tedious').Request;
 var fs = require('fs');
 var util = require('util');
-  
 
 var LOBS = [];
 var Products = [];
 
 // Setup Restify Server
+
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
     console.log('%s listening to %s', server.name, server.url);
 });
 
-// Create connector and listen for messages
 var connector = new builder.ChatConnector({
     appId: process.env.MICROSOFT_APP_ID,
     appPassword: process.env.MICROSOFT_APP_PASSWORD
@@ -26,10 +26,14 @@ var connector = new builder.ChatConnector({
 
 server.post('/api/messages', connector.listen());
 
+
 // Create your bot with a function to receive messages from the user
 // Create bot and default message handler
 var bot = new builder.UniversalBot(connector, function (session) {
+
+
 session.sendTyping();
+
 // Create connection to database
 var config = {
  userName: 'testdbuser', // update me
@@ -61,7 +65,9 @@ result2.push(row[0].value);
 if(result2.length > 0)
 {
 session.send("Hi "+session.message .user.name+" , How are you , Which Line of Business i can help you with today ?");
-session.send("Type LOB to list the available Business areas");
+session.beginDialog('LOBDialog');
+
+//session.send("Type LOB to list the available Business areas");
 session.userData.jolly = "Jolly";
 }
 else
@@ -76,8 +82,8 @@ connection1.execSql(request);
 
 });
 
-bot.set('persistUserData',false);
-bot.set('persistConversationData',false);
+//bot.set('persistUserData',false);
+//bot.set('persistConversationData',false);
 
 // Add dialog to return list of shirts available
 bot.dialog('LOB', function (session) {
@@ -107,26 +113,26 @@ session.beginDialog('LOBDialog');
 bot.dialog('LOBDialog', [
     function (session, args) {
 // Create connection to database
-var config = {
- userName: 'testdbuser', // update me
- password: 'h#^E*9%Jgh', // update me
- server: 'testserverforpoc.database.windows.net', // update me
- options: {
- encrypt: true,
- database: 'TestChatBot', //update me
- rowCollectionOnDone:true, //for large number of data enabling this would result in huge memory usage
- rowCollectionOnRequestCompletion : true
- }
-}
+                        var config = {
+                            userName: 'testdbuser', // update me
+                            password: 'h#^E*9%Jgh', // update me
+                            server: 'testserverforpoc.database.windows.net', // update me
+                            options: {
+                                encrypt: true,
+                                database: 'TestChatBot', //update me
+                                rowCollectionOnDone:true, //for large number of data enabling this would result in huge memory usage
+                                rowCollectionOnRequestCompletion : true
+                            }
+                        }
 var connection1 = new Connection(config);
 
 // Attempt to connect and execute queries if connection goes through
 connection1.on('connect', function(err) {
-if (err) {
-console.log(err)
+    if (err) {
+    console.log(err)
 }
 else{
-queryDatabase()
+    queryDatabase()
 }
 });
 //session.send("Campaigns are available for below products , What product would you like to see today ?");
@@ -135,41 +141,34 @@ function queryDatabase(){
 console.log('Reading rows from the Table...');
 // Read all rows from table
 request = new Request(
-"select ID,Entity_Name from MS_ENTITY where entity_type = 'LOB'",
-function(err, rowCount, rows) {
-console.log(rowCount + ' row(s) returned');
-rows.forEach(function (row){
-var tempcard = new builder.HeroCard(session)
-.buttons([
-builder.CardAction.imBack(session, "Display available LOB", row[1].value)
-]);
-//result2.push(tempcard);
-var obj = {"ID":+row[0].value,"value":row[1].value}
-LOBS.push(
-obj
-);
-//result2.push(JSON.stringify(row, null, 2));
-//console.log(JSON.stringify(row, null, 2));
-});
-// var msg = new builder.Message(session);
-// msg.attachmentLayout(builder.AttachmentLayout.list)
-// msg.attachments(JSON.stringify(rows, null, 2));
-// session.send(msg).endDialog();
-
-builder.Prompts.choice(session, "Display available LOB", LOBS,{ listStyle: builder.ListStyle.button });
-
-}
-);
+                        "select ID,Entity_Name from MS_ENTITY where entity_type = 'LOB'",
+                        function(err, rowCount, rows) {
+                        console.log(rowCount + ' row(s) returned');
+                        LOBS = [];
+                        rows.forEach(function (row){
+                            var tempcard = new builder.HeroCard(session)
+                                            .buttons([
+                                            builder.CardAction.imBack(session, "Display available LOB", row[1].value)
+                                            ]);
+                            var obj = {"ID":+row[0].value,"value":row[1].value}
+                            LOBS.push(
+                            obj
+                            );
+                        });
+                        builder.Prompts.choice(session, "Displaying available LOB", LOBS,{ listStyle: builder.ListStyle.button });
+                        }
+                );
 connection1.execSql(request);
 }
     },
     function (session, results) {
         // Save size if prompted
        if (results.response) {
-var obj = LOBS[results.response.index];
-session.userData.LOB = obj.ID;
-session.beginDialog('LOBButtonClick');
-}
+            var obj = LOBS[results.response.index];
+            session.userData.LOB = obj.ID;
+            session.userData.LOBValue = obj.value;
+            session.beginDialog('LOBButtonClick');
+        }
     }
 ]).triggerAction({ matches: /(Display|Outcome)\s.*Business Area/i });
 
@@ -199,7 +198,7 @@ else{
 queryDatabase()
 }
 });
-session.send("Campaigns are available for below products , What product would you like to see today ?");
+//session.send("Campaigns are available for below products , What product would you like to see today ?");
 session.sendTyping();
 function queryDatabase(){
 console.log('Reading rows from the Table...');
@@ -209,17 +208,18 @@ request = new Request(
 "Select ID,Entity_Name from MS_Entity where Entity_Parent_ID ="+session.userData.LOB,
 function(err, rowCount, rows) {
 console.log(rowCount + ' row(s) returned');
+Products=[];
 rows.forEach(function (row){
 var tempcard = new builder.HeroCard(session)
 .buttons([
-builder.CardAction.imBack(session, "Display available campaign for product - Outlook", row[1].value)
+builder.CardAction.imBack(session, "Display available campaign for product", row[1].value)
 ]);
 var objProd = {"ID":+row[0].value,"value":row[1].value}
 Products.push(objProd);
 //result2.push(tempcard);
 result2.push(row[0].value);
 });
-builder.Prompts.choice(session, "Display available campaign for product - Outlook", Products,{ listStyle: builder.ListStyle.button });
+builder.Prompts.choice(session, "Below products are available for "+session.userData.LOBValue+" line of business", Products,{ listStyle: builder.ListStyle.button });
 }
 );
 connection1.execSql(request);
@@ -230,10 +230,11 @@ connection1.execSql(request);
        if (results.response) {
   var objprod = Products[results.response.index];
 session.userData.product=  objprod.ID;
-session.beginDialog('reporttypeselection');
+//session.beginDialog('reporttypeselection');
+session.beginDialog('CampaignDialog');
 }
     }
-]).triggerAction({ matches: /(Display|Outcome)\s.*LoB/i });
+]).triggerAction({ matches: /^(Product|list)/i });
 
 // choose a campaign
 bot.dialog('CampaignDialog', [
@@ -283,6 +284,43 @@ connection1.execSql(request);
 }
     },
     function (session, results) {
+
+session.beginDialog('reporttypeselection');
+
+
+    }
+]).triggerAction({ matches: /(Display|show)\s.*campaign/i });;;
+
+// Sends attachment inline in base64
+function sendInline(session, filePath, contentType, attachmentFileName) {
+    fs.readFile(filePath, function (err, data) {
+        if (err) {
+            
+        
+            return session.send('Oops. Error reading file.');
+        }
+
+        var base64 = Buffer.from(data).toString('base64');
+
+        var msg = new builder.Message(session)
+            .addAttachment({
+                contentUrl: util.format('data:%s;base64,%s', contentType, base64),
+                contentType: contentType,
+                name: attachmentFileName
+            });
+
+        session.send(msg);
+    });
+}
+
+
+// select a report type - Delivery Health|Outcome
+bot.dialog('reporttypeselection', [
+    function (session, args) {
+builder.Prompts.choice(session, "Following reports are available , What type of report would you like to see today ?", "Delivery Health Report|Outcome Report",{ listStyle: builder.ListStyle.button });
+    },
+    function (session, results) {
+//session.beginDialog('CampaignDialog');
 console.log('selection...' + results.response.entity.toLowerCase()); 
 var titls = results.response.entity;
 // Create connection to database
@@ -381,51 +419,19 @@ session.send(msgs);
 );
 connection1.execSql(request);
 }
+
     }
-]).triggerAction({ matches: /(Display|show)\s.*campaign/i });;;
+]).triggerAction({ matches: /(Display|show)\s.*report/i });;
 
-// Sends attachment inline in base64
-function sendInline(session, filePath, contentType, attachmentFileName) {
-    fs.readFile(filePath, function (err, data) {
-        if (err) {
-            
-        
-            return session.send('Oops. Error reading file.');
-        }
+// bot.dialog('/delete', (session) => {
+// delete session.userData
+// session.endDialog('Everything has been wiped out')
 
-        var base64 = Buffer.from(data).toString('base64');
-
-        var msg = new builder.Message(session)
-            .addAttachment({
-                contentUrl: util.format('data:%s;base64,%s', contentType, base64),
-                contentType: contentType,
-                name: attachmentFileName
-            });
-
-        session.send(msg);
-    });
-}
-
-
-// select a report type - Delivery Health|Outcome
-bot.dialog('reporttypeselection', [
-    function (session, args) {
-builder.Prompts.choice(session, "Following reports are available , What type of report would you like to see today ?", "Delivery Health Report|Outcome Report",{ listStyle: builder.ListStyle.button });
-    },
-    function (session, results) {
-session.beginDialog('CampaignDialog');
-    }
-]).triggerAction({ matches: /(Display|show)\s.*product/i });;
-
-bot.dialog('/delete', (session) => {
-delete session.userData
-session.endDialog('Everything has been wiped out')
-
-})
-.triggerAction({
-matches: /delete all/i,
-confirmPrompt: "This will wipe everything out. Are you sure?"
-});
+// })
+// .triggerAction({
+// matches: /delete all/i,
+// confirmPrompt: "This will wipe everything out. Are you sure?"
+// });
 
 function queryDB(query){
 console.log('Reading rows from the Table...');
